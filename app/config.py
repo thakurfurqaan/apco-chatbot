d3e72a6c9c37e9c7abc3457d1a6a19f9529c26a6
Furqaan Thakur <135686493+furqaanfcs@users.noreply.github.com>
2024-09-01 20:36:28 +0400

    Specified embedding model in settings

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     OPENAI_API_KEY: str = ""
     CHROMA_DB_PATH: str = ""
-    OPENAI_EMBEDDING_MODEL: str = ""
+    OPENAI_EMBEDDING_MODEL: str = "text-embedding-3-large"
     OPENAI_CHAT_MODEL: str = "gpt-4o-mini"
     PRODUCT_VECTOR_STORE_COLLECTION_NAME: str = "products"
 
