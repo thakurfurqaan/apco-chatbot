c6e610758d207665b7aae60b852940eef9063d1f
Furqaan Thakur <135686493+furqaanfcs@users.noreply.github.com>
2024-09-01 20:31:58 +0400

    Added tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,12 @@ client = TestClient(app)
 
 
 def test_chat_endpoint():
-    response = client.post("/chat", json={"message": "Hello, chatbot!"})
+    response = client.post(
+        "/chat",
+        json={
+            "message": "I need a product to control fungal diseases in my wheat crop"
+        },
+    )
     assert response.status_code == 200
     assert "message" in response.json()
 
@@ -15,3 +20,13 @@ def test_products_endpoint():
     response = client.get("/products")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
+
+
+def test_product_sync_endpoint():
+    response = client.post("/products/sync")
+    assert response.status_code == 200
+    assert "message" in response.json()
+    assert (
+        response.json()["message"]
+        == "Successfully updated products in the vector store"
+    )
--- tests/test_services.py
+++ tests/test_services.py
@@ -1,10 +1,7 @@
-import pytest
-
 from app.services.ecommerce.ecommerce_mock.ecommerce_mock import EcommerceMockService
 
 
-@pytest.mark.asyncio
-async def test_ecommerce_service():
+def test_ecommerce_service():
     service = EcommerceMockService()
-    products = await service.get_all_products()
+    products = service.get_all_products()
     assert isinstance(products, list)
