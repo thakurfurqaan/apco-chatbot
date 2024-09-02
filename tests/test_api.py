d53562627054af8903f54f7481521274bdf8c942
Furqaan Thakur <furqaan@practicalimpactservices.com>
2024-09-02 10:02:15 +0400

    Added tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,15 +5,28 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_chat_endpoint():
+def test_chat_endpoint_text_only():
     response = client.post(
         "/chat",
-        json={
+        data={
             "message": "I need a product to control fungal diseases in my wheat crop"
         },
     )
     assert response.status_code == 200
     assert "message" in response.json()
+
+
+def test_chat_endpoint_text_and_file():
+    with open("tests/test-crop-disease-image.webp", "rb") as file:
+        files = {"file": ("test-crop-disease-image.webp", file, "image/webp")}
+        data = {
+            "message": "I need a product to control fungal diseases in my wheat crop"
+        }
+
+        response = client.post("/chat", data=data, files=files)
+
+        assert response.status_code == 200
+        assert "message" in response.json()
 
 
 def test_products_endpoint():
